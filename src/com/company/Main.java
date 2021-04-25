86c0b2978a4ddcd108667eca469e3e66f1f213bf
yaroslav <yaroslavovsienko@gmail.com>
2021-04-25 11:21:09 +0300

    commit4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("hi1");
+        System.out.println("hi1");
+        System.out.println("hi1");
     }
 }
